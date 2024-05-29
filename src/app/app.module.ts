8fc01049754d9d3874e92ac8b770362756c0bf29
Mennatallah Helmy <Mennatallah.Helmy@ad.cyshield>
2024-05-29 11:01:05 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { PageCourseComponent } from './component/page-course/page-course.component';
+import { IntroComponent } from './component/intro/intro.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+ 
+    HeaderComponent,
+    FooterComponent,
+    PageCourseComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/page-course/page-course.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-course',
+  templateUrl: './page-course.component.html',
+  styleUrls: ['./page-course.component.scss']
+})
+export class PageCourseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
